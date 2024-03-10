6f6a35d18ffb619348f75aefe6cd22c73e00fcae
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2024-03-10 12:42:39 +0530

    Add files via upload

--- categories/script.js
+++ categories/script.js
@@ -1,16 +1,9 @@
 
+
  function logout() {
      document.documentElement.setAttribute('data-bs-theme', 'dark');
      window.location.href = "../login/"; // Assuming login.html is in the login folder
  }
- function mode() 
- {
-     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-         document.documentElement.setAttribute('data-bs-theme', 'light')
-         document.getElementById('light').innerText = "Dark Mode"
-     } else {
-         document.documentElement.setAttribute('data-bs-theme', 'dark')
-         document.getElementById('light').innerText = "Light Mode"
-     }
- }
+ 
+
  
