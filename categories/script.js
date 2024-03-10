c0f474658312848c21f94d299a4afc92058aec4b
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2024-03-10 12:36:43 +0530

    Add files via upload

--- categories/script.js
+++ categories/script.js
@@ -3,7 +3,8 @@
      document.documentElement.setAttribute('data-bs-theme', 'dark');
      window.location.href = "../login/"; // Assuming login.html is in the login folder
  }
- document.getElementById('light').addEventListener('click', () => {
+ function mode() 
+ {
      if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
          document.documentElement.setAttribute('data-bs-theme', 'light')
          document.getElementById('light').innerText = "Dark Mode"
@@ -11,6 +12,5 @@
          document.documentElement.setAttribute('data-bs-theme', 'dark')
          document.getElementById('light').innerText = "Light Mode"
      }
- })
-
+ }
  
